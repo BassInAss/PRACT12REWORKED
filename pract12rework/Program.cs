75a03281c958cd6a950aab6da7061de78fb7d0f6
VikTor <viktor.kononenko.2003@mail.ru>
2022-01-19 10:38:08 +0500

    added menu

---
+++ pract12rework/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("-ввести А");
+Console.WriteLine("-ввести В");
+Console.WriteLine("-выполнить операцию “+”");
+Console.WriteLine("-выполнить операцию “-”");
+Console.WriteLine("-выполнить операцию “*”");
+Console.WriteLine("-выполнить операцию “/”");
+
+
+
+
+
+
